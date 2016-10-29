eded9752dcea69bf605d395be5614ba22bb2a291
Morhaus <alexandre.kirszenberg@gmail.com>
2016-10-29 20:22:24 +0200

    NN Perf improvements

--- src/test/test_network.c
+++ src/test/test_network.c
@@ -54,7 +54,7 @@ void test_network_output(network* nt, double* input)
 void test_network_sgd()
 {
   size_t sizes[] = { 2, 2, 1 };
-	network* nt = create_network(sizes, 3);
+  network* nt = create_network(sizes, 3);
 
   training_datum* datum1 = create_training_datum(
     (double[]){ 0, 0 },
@@ -97,8 +97,67 @@ void test_network_sgd()
   printf("sgd: ok\n");
 }
 
+void test_network_failure_rate(unsigned epochs)
+{
+  size_t sizes[] = { 2, 3, 1 };
+
+  training_datum* datum1 = create_training_datum(
+    (double[]){ 0, 0 },
+    (double[]){ 0 }
+  );
+  training_datum* datum2 = create_training_datum(
+    (double[]){ 1, 0 },
+    (double[]){ 1 }
+  );
+  training_datum* datum3 = create_training_datum(
+    (double[]){ 0, 1 },
+    (double[]){ 1 }
+  );
+  training_datum* datum4 = create_training_datum(
+    (double[]){ 1, 1 },
+    (double[]){ 0 }
+  );
+  training_datum* data[] = { datum1, datum2, datum3, datum4 };
+
+  unsigned epochs_failed = 0;
+  double* activations = malloc(1 * sizeof (double));
+  for (unsigned i = 0; i < epochs; ++i)
+  {
+    int epoch_failed = 0;
+    double a, b, c, d;
+  	network* nt = create_network(sizes, 3);
+    sgd(nt, data, 4, 10000, 4, 10.);
+    feedforward(nt, (double[]){ 0, 0 }, activations);
+    a = activations[0];
+    feedforward(nt, (double[]){ 1, 0 }, activations);
+    b = activations[0];
+    feedforward(nt, (double[]){ 0, 1 }, activations);
+    c = activations[0];
+    feedforward(nt, (double[]){ 1, 1 }, activations);
+    d = activations[0];
+    epoch_failed = a > 0.3 || b < 0.7 || c < 0.7 || d > 0.3;
+    epochs_failed += epoch_failed;
+    if (epoch_failed)
+    {
+      printf("epoch failed: %f %f %f %f\n", a, b, c, d);
+      printf("%f %u/%u\n", (double)epochs_failed / (double)(i + 1) * 100. , i, epochs);
+    }
+    free_network(nt);
+  }
+
+  free(activations);
+  free(datum1);
+  free(datum2);
+  free(datum3);
+  free(datum4);
+
+  printf("epochs failed: %u/%u\n", epochs_failed, epochs);
+}
+
 void perf_xor(unsigned epochs)
 {
+	srand(42); // Unrandomize the seed.
+
   size_t sizes[] = { 2, 2, 1 };
 
   training_datum* datum1 = create_training_datum(
@@ -123,7 +182,7 @@ void perf_xor(unsigned epochs)
   for (size_t i = 0; i < epochs; ++i)
   {
     network* nt = create_network(sizes, 3);
-    sgd(nt, data, 4, 10000, 4, 10.);
+    sgd(nt, data, 4, 1000, 4, 10.);
     free_network(nt);
   }
 
@@ -141,9 +200,11 @@ void perf_xor(unsigned epochs)
 
 void test_network()
 {
-	srand(42); // Randomize the seed.
+	// srand(time(NULL)); // Randomize the seed.
+	srand(44); // Randomize the seed.
 
-  perf_xor(100);
+  // perf_xor(100);
   // test_network_feedforward();
-  // test_network_sgd();
+  test_network_sgd();
+  // test_network_failure_rate(100000);
 }
