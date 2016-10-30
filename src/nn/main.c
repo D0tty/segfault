0e7da7de136ace4ba7cb70f06ab783e4753f6790
Thomas Michelot <thomas.michelot@epita.fr>
2016-10-30 18:53:07 +0100

    rmsp

--- src/nn/main.c
+++ src/nn/main.c
@@ -12,7 +12,7 @@ int main(/*int argc, char *argv[]*/)
 
   size_t sizes[] = { 2, 3, 1 };
 	network* nt = create_network(sizes, 3);
-  
+
   print_network(nt, 1);
 
   save_network(nt,"save.txt");
@@ -21,6 +21,6 @@ int main(/*int argc, char *argv[]*/)
   nt = load_network("save.txt");
   save_network(nt,"save_after_laod.txt");
   free_network(nt);
-  
+
   return 0;
 }
