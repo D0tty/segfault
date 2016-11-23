bd2d89af83a76de108f331326f827af0086d2be0
Mickael El Hawat <mickael.el_hawat@epita.fr>
2016-11-23 19:45:10 +0100

    faut commit et push mika

---
+++ src/gtk/memomika.c
@@ -0,0 +1,9 @@
+/*faire les includes*/
+
+/*pour assigner au boutons les fonctions de test*/
+
+/*clik sur le bouton -> signal et dans activated mettre le nom de la fonction 
+  a activé*/
+
+/*plus de couleur pimper le truc*/
+
