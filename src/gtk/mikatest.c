f2b604d76f110eccf37d6397ad8f35580c5b49f5
Mickael El Hawat <mickael.el_hawat@epita.fr>
2016-12-02 01:08:01 +0100

    j ai pas rendu ca

--- src/gtk/mikatest.c
+++ src/gtk/mikatest.c
@@ -8,28 +8,6 @@ void quit (GtkToggleButton *tbutton, gpointer data)
   gtk_main_quit ();
 }
 
-/*trouve le chemin*/ 
-/*gchar* load(gchar* path)                                                        
-{                                                                               
-  GtkWidget *dialog = gtk_file_chooser_dialog_new("Load image", NULL,           
-  GTK_FILE_CHOOSER_ACTION_OPEN, GTK_STOCK_CANCEL,                               
-  GTK_RESPONSE_CANCEL, GTK_STOCK_OPEN, GTK_RESPONSE_ACCEPT, NULL);              
-  if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT)                
-  {                                                                             
-    path = gtk_file_chooser_get_preview_filename (                              
-    GTK_FILE_CHOOSER(dialog));                          
-    GdkPixbuf *pix = gdk_pixbuf_new_from_file (path, NULL);                     
-    pix = gdk_pixbuf_scale_simple (pix, 700, 850, GDK_INTERP_NEAREST);          
-    GtkWidget *image = gtk_image_new_from_pixbuf(pix);                          
-    GtkWidget *win = gtk_window_new(GTK_WINDOW_TOPLEVEL);                       
-    gtk_window_set_title(GTK_WINDOW(win), path);                                
-    gtk_container_add(GTK_CONTAINER(win), image);                               
-    gtk_window_move(GTK_WINDOW(win), 500, 0);                                   
-    gtk_widget_show_all(win);                                                   
-  }                                                                           
-  gtk_widget_destroy (dialog);                                        
-  return path;
-}*/
 
 //ouvrir le dialogue
 void ouvrir(GtkWidget *button, GtkWidget *imgbox)
@@ -37,37 +15,6 @@ void ouvrir(GtkWidget *button, GtkWidget *imgbox)
   char* filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(button));
   gtk_image_set_from_file(GTK_IMAGE(imgbox), filename);
 }
-/*
-void charger (GtkWidget *dialog, GTK_IMAGE *img)
-{
-
-
-}*/
-
-//choisir l'image
-/*void choose(GtkWidget *button ,GtkWidget *dial)
-{
-  GtkWidget *dialog;
-  GtkFileChooserAction action = GTK_FILE_CHOOSER_ACTION_OPEN;
-  gint res;
-      
-  dialog = dial; 
-  dialog = gtk_file_chooser_dialog_new ("Open File",NULL,
-      action,("_Cancel"),GTK_RESPONSE_CANCEL,("_Open"),
-      GTK_RESPONSE_ACCEPT,NULL);
-          
-  res = gtk_dialog_run (GTK_DIALOG (dialog));
-  if (res == GTK_RESPONSE_ACCEPT)
-  {
-    char *filename;
-    GtkFileChooser *chooser = GTK_FILE_CHOOSER (dialog);
-    //path = gtk_file_chooser_get_filename (chooser);
-    //load(path);
-    gtk_image_set_from_file (GTK_IMAGE(image) , filename);
-    g_free (filename);
-  }
-  gtk_widget_destroy (dialog);
-}*/
 
 GtkBuilder* get_build()
 {
@@ -78,12 +25,6 @@ GtkBuilder* get_build()
   }
   return build;
 }
-/*
-void testit(GtkToggleButton *GTKButton, gpointer data)
-{
-  printf("YOU DID IT !!!\n");
-  gtk_main_quit();
-}*/
 
 int main (int argc, char *argv[])
 {
